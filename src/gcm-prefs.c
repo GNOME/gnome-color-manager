fa6d568ac7a7258756793631b1106264c00f3515
Richard Hughes <richard@hughsie.com>
2010-04-26 23:20:05 +0100

    trivial: fix up three potential crashers were error was not being used and then dereferenced

--- src/gcm-prefs.c
+++ src/gcm-prefs.c
@@ -2378,7 +2378,7 @@ gcm_prefs_startup_phase1_idle_cb (gpointer user_data)
 
 	/* setup CMYK combobox */
 	widget = GTK_WIDGET (gtk_builder_get_object (builder, "combobox_space_cmyk"));
-	colorspace_cmyk = gconf_client_get_string (gconf_client, GCM_SETTINGS_COLORSPACE_CMYK, NULL);
+	colorspace_cmyk = gconf_client_get_string (gconf_client, GCM_SETTINGS_COLORSPACE_CMYK, &error);
 	if (colorspace_cmyk == NULL) {
 		egg_warning ("failed to get configuration value: %s", error->message);
 		g_clear_error (&error);
@@ -2391,7 +2391,7 @@ gcm_prefs_startup_phase1_idle_cb (gpointer user_data)
 	/* setup rendering lists */
 	widget = GTK_WIDGET (gtk_builder_get_object (builder, "combobox_rendering_display"));
 	gcm_prefs_set_combo_simple_text (widget);
-	intent_display = gconf_client_get_string (gconf_client, GCM_SETTINGS_RENDERING_INTENT_DISPLAY, NULL);
+	intent_display = gconf_client_get_string (gconf_client, GCM_SETTINGS_RENDERING_INTENT_DISPLAY, &error);
 	if (intent_display == NULL) {
 		egg_warning ("failed to get configuration value: %s", error->message);
 		g_clear_error (&error);
@@ -2402,7 +2402,7 @@ gcm_prefs_startup_phase1_idle_cb (gpointer user_data)
 
 	widget = GTK_WIDGET (gtk_builder_get_object (builder, "combobox_rendering_softproof"));
 	gcm_prefs_set_combo_simple_text (widget);
-	intent_softproof = gconf_client_get_string (gconf_client, GCM_SETTINGS_RENDERING_INTENT_SOFTPROOF, NULL);
+	intent_softproof = gconf_client_get_string (gconf_client, GCM_SETTINGS_RENDERING_INTENT_SOFTPROOF, &error);
 	if (intent_softproof == NULL) {
 		egg_warning ("failed to get configuration value: %s", error->message);
 		g_clear_error (&error);
