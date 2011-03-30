72809cba338671f5e961f843816df8cb90800c79
Kjartan Maraas <kmaraas@gnome.org>
2011-03-30 12:42:17 +0200

    Revert accidental commit pending freeze thaw

--- libcolor-glib/gcm-debug.h
+++ libcolor-glib/gcm-debug.h
@@ -28,6 +28,8 @@
 
 #include <glib.h>
 
+#include "config.h"
+
 gboolean	 gcm_debug_is_verbose		(void);
 GOptionGroup	*gcm_debug_get_option_group	(void);
 void		 gcm_debug_setup		(gboolean	 enabled);
