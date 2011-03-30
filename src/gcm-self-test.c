f45993ba957f08ec3d60654022ae9b8b6be14f56
Kjartan Maraas <kmaraas@gnome.org>
2011-03-30 12:39:24 +0200

    include <config.h> in .c files and remove it from headers.

--- src/gcm-self-test.c
+++ src/gcm-self-test.c
@@ -19,6 +19,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+#include "config.h"
+
 #include <glib-object.h>
 #include <math.h>
 #include <glib/gstdio.h>
